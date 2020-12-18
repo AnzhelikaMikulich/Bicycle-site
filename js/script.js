c9cab3993c865103409bb2724f9d173d3119a4bc
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2020-12-18 11:52:41 +0300

    fixed bugs and edited the code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let header__burger = document.querySelector('.icon-menu');
-let header_menu = document.querySelector('.menu_body');
-let back = document.querySelector('body');
-let header__list = document.querySelector('.menu_list');
+const header__burger = document.querySelector('.icon-menu');
+const header_menu = document.querySelector('.menu_body');
+const back = document.querySelector('body');
+const header__list = document.querySelector('.menu_list');
 
 header__burger.onclick = function(){
     header__burger.classList.toggle('active');
@@ -13,3 +13,19 @@ header__list.onclick = function () {
     header__list.classList.remove('active');
     back.classList.toggle('lock');
 }
+
+$(document).ready(function(){
+    $('.slider').slick(
+        {
+     arrows: true,
+     dots: true,
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     speed:1000,
+     easing: 'easy',
+     centerMode: true,
+     asNavFor: '.sliderbig'
+   }
+   )
+})
+
